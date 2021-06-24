0f34ac91829e8a2547e099e5b6346788bb423b01
Dmitriy Pleshevskiy <dmitriy@ideascup.me>
2021-06-24 21:59:15 +0300

    chore: fix use client response type

--- src/client_hook.ts
+++ src/client_hook.ts
@@ -3,5 +3,5 @@ import { useRequestContext } from './request_context';
 export function useClient() {
     const { client } = useRequestContext();
 
-    return [client];
+    return [client] as const;
 }
